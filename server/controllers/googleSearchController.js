2c7db857c31d63c96cb886c7152ed1d36700592d
Pablo Mangione <mangione.mkt@gmail.com>
2017-12-17 01:27:18 +0100

    added pagination

--- server/controllers/googleSearchController.js
+++ server/controllers/googleSearchController.js
@@ -4,19 +4,25 @@ const client = new GoogleImages(process.env.CSE_ID,process.env.GOOGLE_KEY)
 const SearchClient = {}
 
 SearchClient.searchImages = (req,res) => {
-	let { query } = req.params
-
-	client.search(query)
+	let query = req.params.query
+	let numberOfResults = req.params.numberOfResults || 1
+	client.search(query, {page:numberOfResults})
 		.then(images => {
+			let results = []
 			images.map(image => {
-				res.json({
+				results.push({
 					"url":image.url,
 					"type":image.type,
 					"width/height":image.width+"x"+image.height,
 					"thumbnail":image.thumbnail.url
 				})
 			})
+
+			res.json({
+				"results":results
+			})
 		})
+		.catch(console.error)
 }
 
 module.exports = SearchClient
--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,6 @@ const helloWorld = require('./controllers/helloWorld')
 const googleSearchController = require('./controllers/googleSearchController')
 
 routes.get("/", helloWorld.get)
-routes.get("/search/:query", googleSearchController.searchImages)
+routes.get("/search/:query/:numberOfResults?", googleSearchController.searchImages)
 
 module.exports = routes
